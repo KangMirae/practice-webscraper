aad3312b2c35826f30253120d4df312023e48650
Mirae Kang <miraemi01@gmail.com>
2026-02-01 21:36:11 +0100

    add ecommerce command

---
+++ scraper/management/commands/scraper_ecommerce.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from scraper.services.scraper_ecommerce import scrape_ecommerce_site
+from scraper.models import EcommerceProduct 
+
+class Command(BaseCommand):
+    help = "webscraper.io 쇼핑몰 데이터를 긁어옵니다."
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write("스크래핑 시작...")
+        
+        data = scrape_ecommerce_site()
+        
+        for item in data:
+            EcommerceProduct.objects.create(
+                title=item["title"],
+                price=item["price"],
+                description=item["description"],
+                url=item["url"]
+            )
+            
+        self.stdout.write(self.style.SUCCESS(f"{len(data)}개의 상품 정보를 저장했습니다!"))
